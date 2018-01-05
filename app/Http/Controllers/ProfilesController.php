adf9e38cbf633d38a77abb06caee0950b5156735
Franky Tse <TseJianJi@Frankys-MBP.lan>
2018-01-05 16:24:18 +0100

    Extra functionaliteit: Search post

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -101,6 +101,8 @@ class ProfilesController extends Controller
 
         $user->profile->youtube = $request->youtube;
 
+        $user->profile->about = $request->about;
+
 
         $user->save();
 
